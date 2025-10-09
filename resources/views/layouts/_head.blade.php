ce6fe1360233761d0fb1ddeac9e6689111be7819
Izza Wildan Ridhoni <izza.wildan@ptsji.id>
2025-10-09 14:30:24 +0700

    update title

--- resources/views/layouts/_head.blade.php
+++ resources/views/layouts/_head.blade.php
@@ -1,6 +1,6 @@
 <meta charset="utf-8">
 <meta content="width=device-width, initial-scale=1.0" name="viewport">
-<title>Index - FolioOne Bootstrap Template</title>
+<title>Izza Wildan</title>
 <meta name="description" content="">
 <meta name="keywords" content="">
 
